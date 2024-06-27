90b0dbcf8cdd9ef4f61d3ebb1c4b247b398348c8
Miakosa <130900759+Miakosa@users.noreply.github.com>
2024-06-27 19:48:26 +0200

    Add files via upload
    PDcurses

--- Snake/Snake/Programm.cpp
+++ Snake/Snake/Programm.cpp
@@ -1,25 +1,27 @@
-#include <iostream>
-#include "Window.h"
 
+#include <curses.h>
 
-int main() {
-	std::cout << " Create Window\n";
+int main()
+{
+    initscr();                      // initialise Bib
+   
 
-		Window* pWindow = new Window(); // Pointer
-		bool running = true;
-		while (running) {
-			
-			if (!pWindow->ProcessMessages())
-			{
-				std::cout << "Closing Window\n";
-				running = false;
-			}
+    start_color();                  // activate Colorfunction
+    init_pair(1, COLOR_BLUE, COLOR_WHITE);  // define a Color - Pair 1
+    init_pair(2, COLOR_WHITE, COLOR_BLUE);
+    bkgd(COLOR_PAIR(2));    // Color Main-Window set to ´Pair 2
 
-			// "Render"
-			//
-			Sleep(10);
-		}
-		delete pWindow;
+    
+    WINDOW* window = newwin(12, 12, 1, 1); // create Window 
+    wbkgd(window, COLOR_PAIR(1));   // Color window set to Pair 1
+    mvwprintw(window, 1, 1, "Hello"); 
+    wrefresh(window); // window refresh
 
-		return 0;
+    printw("Hello World !!!");      // print that
+    refresh();  // refresh main Window
+    getch(); // wait for user input
+    delwin(window); // delete window
+    endwin();  // end 
+
+    return 0;
 }
